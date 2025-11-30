7fe9cf8886631231882315274ad09b9ab723c29f
Khashayar Khosrosourmi <ksourmi@gmail.com>
2025-11-30 21:20:51 +0200

    add contact api

--- portfolio/api.py
+++ portfolio/api.py
@@ -1,9 +1,11 @@
 from ninja import NinjaAPI
 from blog.api import router as blog_router
 from project.api import router as project_router
+from contact.api import router as contact_router
 
 
 api = NinjaAPI(version="v0.1", urls_namespace="api")
 
 api.add_router("/blog/", blog_router)
 api.add_router("/project/", project_router)
+api.add_router("/contact/", contact_router)
